3af16cf72eb84c95fe727a1c2b9c570331f4c371
Himanshu-dev-web <hims213174@gmail.com>
2024-01-28 17:43:35 +0530

    done...

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,4 @@
-import React,{useState,CSSProperties } from 'react'
+import React,{useState,CSSProperties, useEffect } from 'react'
 import { Select, Typography,Row,Col, Avatar, Card } from 'antd'
 import { useGetCryptosQuery } from '../services/cryptoApi';
 import { useGetNewsQuery } from '../services/cryptoNewsApi'
@@ -17,39 +17,86 @@ const override: CSSProperties = {
 const { Text, Title} = Typography;
 const demoImage = "https://www.bing.com/th?id=ODF.2UiZPknOYXZPC3V72DJ1Rw&pid=news"
 const News = ({simplified}) => {
+    const [newsCategory, setNewsCategory] = useState('Ethereum');
+    const [cryptoNews, setCryptoNews] = useState([]);
 
-    const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
+    const fetchData = async () => {
+        try {
+            const params = new URLSearchParams({
+              query: JSON.stringify({
+                $query: {
+                  $and: [
+                    {
+                      conceptUri: `http://en.wikipedia.org/wiki/${newsCategory}`,
+                    },
+                    {
+                      lang: 'eng',
+                    },
+                  ],
+                },
+                $filter: {
+                  forceMaxDataTimeWindow: '31',
+                },
+              }),
+              resultType: 'articles',
+              articlesSortBy: 'date',
+              apiKey: 'a453dda7-b27d-4db8-95cb-1d8e927a697e',
+            });
+        
+    
+          const url = `https://newsapi.ai/api/v1/article/getArticles?${params}`;
+    
+          const response = await fetch(url);
+    
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+    
+          const data = await response.json();
+    
+         
+          setCryptoNews(data.articles.results);
+          console.log(cryptoNews);
+        } catch (error) {
+          console.error(error.message);
+        }
+      };
+        useEffect(() => {
+            
+        fetchData();
+        }, [newsCategory]);
+      
+    
+//     const {data: cryptoNews ,isFetching } = useGetNewsQuery({ newsCategory , count:simplified ? 6 :100});
+      const { data } = useGetCryptosQuery(100);
 
-    const {data: cryptoNews ,isFetching } = useGetNewsQuery({ newsCategory , count:simplified ? 6 :100});
-    const { data } = useGetCryptosQuery(100);
+//    console.log(cryptoNews);
 
-
-
-    if(isFetching) return <Audio
-    height = "150"
-    width = "150"
-    radius = "9"
-    color = 'green'
-    ariaLabel = 'five-dots-loading'     
-    wrapperStyle
-    wrapperClass
-  />
+//     if(isFetching) return <Audio
+//     height = "150"
+//     width = "150"
+//     radius = "9"
+//     color = 'green'
+//     ariaLabel = 'five-dots-loading'     
+//     wrapperStyle
+//     wrapperClass
+//   />
 
 
     return (
         
       <>
        <motion.div
-      className="box"
-      initial={{ opacity: 0, scale: 0.5 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{
+        className="box"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
         duration: 0.8,
         delay: 0.3,
         ease: [0, 0.1, 0.1, 0.01]
       }}
     >
-      <Row gutter={[24,24]}>
+      <Row gutter={[24,24]} >
       {!simplified && (
             <Col span={24}>
                 <Select showSearch
@@ -64,28 +111,32 @@ const News = ({simplified}) => {
                 </Select>
             </Col>   
         )}
-                { cryptoNews?.value.map((news,i) => (
+                {cryptoNews.length >0  ? (  cryptoNews?.map((news,i) => (
                         <Col xs={24} sm={12} lg={8} key={i}>
                             <Card hoverable className='news-card'>
-                                <a href={news.url} target="_blank" rel="noreferrer">
+                                <a href={news?.url} target="_blank" rel="noreferrer">
                                     <div className='news-image-container'>
-                                        <Title className='news.title' level={4}>{news.name}</Title>
-                                        <img style={{ maxwidht:'200px', maxHeight:'100px' }} src={news?.image?.thumbnail?.contentUrl || demoImage} alt="img not found"/>
+                                        <Title className='news.title' level={4}>{news?.title}</Title>
+                                        <img style={{ maxwidht:'150px', maxHeight:'100px' }} src={news?.image || demoImage} alt="img not found"/>
                                     </div>
                                     <p>
-                                        {news.description > 100 ? `${news.description.substring(0,100)}...`: news.description }
+                                        {news?.description > 100 ? `${news?.body.substring(0,100)}...`: news?.description }
                                     </p>
                                     <div className='provider-container'>
-                                        <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} />
-                                        <Text className='provider-name' >{news.provider[0]?.name}</Text>
+                                        {/* <Avatar src={news?.provider[0]?.image?.thumbnail?.contentUrl || demoImage} /> */}
+                                        <Text className='provider-name' >{news?.source.title}</Text>
                                     </div>
-                                    <Text >{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+                                    <Text >{moment(news.date).startOf('ss').fromNow()}</Text>
 
                                 </a>
                             </Card>
                         </Col>
-                    ))
-                }
+                    ))) : (
+        <h1>No data available for this coin kindly select other coin</h1>
+      )}
+
+
+              
       </Row>
       </motion.div>
       </>
